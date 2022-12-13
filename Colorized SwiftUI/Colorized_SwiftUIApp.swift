c77780cee534298712a1362c680a7b9fbc18986b
Sav1taRG <sav1tar@me.com>
2022-12-13 18:06:08 +0200

    Initial Commit

---
+++ Colorized SwiftUI/Colorized_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorized_SwiftUIApp.swift
+//  Colorized SwiftUI
+//
+//  Created by Roman on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Colorized_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
